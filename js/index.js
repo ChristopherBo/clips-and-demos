b3b0d1e953874298fa0cfda2df30c4de4766d0a4
ChristopherBo <csb3946@g.rit.edu>
2023-09-02 19:47:46 -0400

    clean up filenames and contents

--- js/index.js
+++ js/index.js
@@ -46,107 +46,6 @@ function lightbox2_close() {
 	document.getElementById('fade2').style.display = 'none';
 }
 
-//The following variables are only to load for reset_profile,
-//NOT for update_profile().
-var profile_name;
-var profile_height;
-var profile_weight;
-var profile_weight_goal;
-var profile_birthdate;
-function load_profile() {
-	//TODO: Loads profile stats from the database and saves them to
-	//their respective variables (listed above this function).
-	//Form variables to load into:
-	//	name: document.getElementById('profile-name-input').value
-	//	height: document.getElementById('profile-height-input').value
-	//	weight: document.getElementById('profile-weight-input').value
-	//	weight goal: document.getElementById('weight-goal-text').innerHTML
-	//	date: document.getElementById('profile-birthdate-input').value
-}
-
-function update_profile() {
-	//TODO: Update profile data based on current info in the form.
-	
-	
-	
-	//Update local variables for reset_profile.
-	profile_name = document.getElementById('profile-name-input').value;
-	profile_height = document.getElementById('profile-height-input').value;
-	profile_weight = document.getElementById('profile-weight-input').value;
-	profile_weight_goal = document.getElementById('weight-goal-text').innerHTML;
-	profile_birthdate = document.getElementById('profile-birthdate-input').value;
-}
-
-function reset_profile() {
-	//Reset profile with the vars initialized from load_profile.
-	if(profile_name == null) { 
-		console.log("Error: load_profile did not initialize variables!"); 
-	} else {
-		document.getElementById('profile-name-input').value = profile_name;
-		document.getElementById('profile-height-input').value = profile_height;
-		document.getElementById('profile-weight-input').value = profile_weight;
-		document.getElementById('weight-goal-text').innerHTML = profile_weight_goal;
-		document.getElementById('profile-birthdate-input').value = profile_birthdate;
-	}
-}
-
-function export_data() {
-	//Exports data via the params specified by our form.
-	//If invalid input, give error text and don't close window.
-	let file_format = document.querySelectorAll('input[name="file-format"]:checked')[0].value;
-	let exports = document.querySelectorAll('input[name="export-type"]:checked');
-	
-	if(exports.length == 0) {
-		//Give error text- they need to select something to export
-		document.getElementById('error-text').innerHTML = "Error: No selected data to export!";
-		document.getElementById('error-text').style.display = "block";
-	} else {
-		//TODO: Start exporting
-		
-		
-		lightbox2_close();
-	}
-}
-
-//when import history button is clicked, click hidden file input button
-$("#import-history-btn").click(function(e){
-	e.preventDefault();
-	$("#import-history-file").trigger('click');
-});
-//hide the import history file input button
-$("#import-history-file").css('opacity','0');
-$("#import-history-file").css('filter','alpha(opacity = 0');
-
-//when a file is uploaded, start importing it's data.
-$("#import-history-file").on('change', import_history);
-
-function import_history() {
-	//Imports history data from a file.
-	//Make sure it's a csv, xml, or json
-	var file = document.getElementById('import-history-file').files[0];
-	if (file) {
-		console.log("File to import: " + file.name);
-	}
-	var extension = file.name.substr(file.name.lastIndexOf('.') + 1);
-	console.log(['csv','xml', 'json'].includes(extension));
-	if(['csv','xml', 'json'].includes(extension) == false) {
-		//Generate error message- wrong file format
-		document.getElementById('error-import-text').innerHTML = "Error: Incorrect file import type!";
-		document.getElementById('error-import-text').style.display = "block";
-		setTimeout( ()=> {
-			document.getElementById('error-import-text').innerHTML = "";
-			document.getElementById('error-import-text').style.display = "none";
-		}, 5000)
-	} else {
-		//TODO: Import history data now
-	}
-}
-
-function load_history() {
-	//TODO: Loads existing history data from the database into the History table.
-	document.getElementById('history-list').style = "display: table";
-}
-
 //Ingredient/Shopping List tb switcher
 function switchTab(evt, tabName) {
   var i, tabcontent, tablinks;
@@ -185,9 +84,9 @@ var TableFilter = (function() {
 
 		function filter(row) {
 			var text = row.textContent.toLowerCase();
-       //console.log(text);
-      var val = input.value.toLowerCase();
-      //console.log(val);
+			//console.log(text);
+			var val = input.value.toLowerCase();
+			//console.log(val);
 			row.style.display = text.indexOf(val) === -1 ? 'none' : 'table-row';
 		}
 
@@ -201,14 +100,6 @@ var TableFilter = (function() {
 		};
  
 	})();
-
-  /*console.log(document.readyState);
-	document.addEventListener('readystatechange', function() {
-		if (document.readyState === 'complete') {
-      console.log(document.readyState);
-			TableFilter.init();
-		}
-	}); */
   
  TableFilter.init(); 
 })();
